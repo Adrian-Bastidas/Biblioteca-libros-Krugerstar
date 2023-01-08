bb5890d5178c09d20629b20e881acf3ced4941be
magobasti <abastidas94@gmail.com>
2023-01-08 12:43:51 -0500

    Reestructuración y arreglo de proyecto dañado

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Index from "./screens/index";
+import Create from "./screens/create";
+import View from "./screens/view";
+import "./App.css";
+import { Routes, Route} from "react-router-dom";
+import Store from "./store/store";
 
 function App() {
   return (
+    <Store>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="create" element={<Create />} />
+        <Route path="view/:bookId" element={<View />} />
+      </Routes>
     </div>
+  </Store>
   );
 }
 
